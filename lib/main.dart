2a426ce4582c00a2956eab5dd4644daf0f7444f8
Muhammed Muneef M <md.muneefnk@gmail.com>
2023-11-08 16:35:06 +0530

    home

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:rent_cruise/view/home_screen/home_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,9 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        body: Container(),
-      ),
+      home: Homescreen(),
     );
   }
 }
