56766e3e484350eccf816d68b46653bbcc4ba9ee
Muhammed Muneef M <md.muneefnk@gmail.com>
2023-11-09 11:19:10 +0530

    rr fixed

--- lib/main.dart
+++ lib/main.dart
@@ -12,11 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-
-      home: Scaffold(
-        backgroundColor: Colors.amber,
-        body: Container(),
-      ),
-    
+      home: Homescreen(),
+    );
   }
 }
