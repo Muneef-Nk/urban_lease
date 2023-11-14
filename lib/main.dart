9550b0e1b9314950a2592404d09114850e617bd9
Ashna Sathar <ashnasathar@gmail.com>
2023-11-14 10:46:14 +0530

    workinggg

--- lib/main.dart
+++ lib/main.dart
@@ -2,20 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:rent_cruise/controller/login_controller/login_controller.dart';
 
-
-import 'package:provider/provider.dart';
 import 'package:rent_cruise/view/Profile/profile..dart';
-import 'package:rent_cruise/view/home_screen/home_screen.dart';
 
 import 'package:rent_cruise/service/location_service.dart';
-import 'package:rent_cruise/view/bottom_navigation/bottom_navigation.dart';
-import 'package:rent_cruise/view/edit_profile/edit_profile.dart';
-import 'package:rent_cruise/view/location/location_screen.dart';
-
-
-import 'package:rent_cruise/service/location_service.dart';
-
-import 'package:rent_cruise/view/splash_screen/splash_screen.dart';
 
 void main() {
   runApp(const MyApp());
